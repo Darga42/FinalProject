2c2c59ee8984dd1df16df162ac088ae32792adeb
Tahir Darga <tahirdarga42@hotmail.com>
2023-06-14 23:03:49 +0300

    10 .Gün Ders Sonu

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //This Result(Kendi bulunduğu class'tan bahsediyor)
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        //Get'ler Read Only oldukları için constructor'da set edilebilir ***
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
